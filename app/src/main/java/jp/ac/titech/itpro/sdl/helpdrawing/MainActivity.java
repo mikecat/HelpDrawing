7f209de78e9000d30c364577d6f4f86ef27719ab
MikeCAT <mikecat@users.noreply.github.com>
2017-07-12 20:44:32 +0900

    Display camera image
    Still many problems left. Some of them are:
    * Camera permission is not requested
    * Orientation support is limited
    * Bad focus

--- app/src/main/java/jp/ac/titech/itpro/sdl/helpdrawing/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/helpdrawing/MainActivity.java
@@ -1,13 +1,69 @@
 package jp.ac.titech.itpro.sdl.helpdrawing;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class MainActivity extends Activity implements TextureView.SurfaceTextureListener {
+    private Camera camera;
+    private TextureView cameraTextureView;
+    private boolean cameraOk = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cameraTextureView = (TextureView)findViewById(R.id.cameraTextureView);
+        cameraTextureView.setSurfaceTextureListener(this);
+        camera = Camera.open();
+        camera.setDisplayOrientation(90);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (cameraOk) camera.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (cameraOk) camera.stopPreview();
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        try {
+            camera.setPreviewTexture(surface);
+            camera.startPreview();
+            cameraOk = true;
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(), R.string.camera_open_fail, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        camera.stopPreview();
+        camera.release();
+        cameraOk = false;
+        return true;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
     }
 }
